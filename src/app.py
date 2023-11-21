0279db4c0ef0b6205c717397e77db3ed353f118d
pharr117 <pjharral@gmail.com>
2023-11-20 19:04:45 -0500

    Add slack basic config and channel setting/validation

--- src/app.py
+++ src/app.py
@@ -2,6 +2,14 @@ import argparse
 from config import Config
 from chain_registry import ChainRegistry
 
+import logging
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from pprint import pprint
+
+logger = logging.getLogger(__name__)
+
+
 def get_app_args():
     parser = argparse.ArgumentParser(description="Cosmos proposal Slack monitor")
     parser.add_argument('--config', default='./config.json')
@@ -14,10 +22,24 @@ def main():
 
     chain_registry = ChainRegistry(zip_location=config.chain_registry_zip_location)
 
-    for chain in config.chains["mainnet"]:
-        chain = chain_registry.get_chain(chain)
-        print(chain.get_healthy_rest_servers())
+    client = WebClient(token=config.slack_bot_token, logger=logger)
+    
+    configured_channel = None
 
+    for result in client.conversations_list(types="public_channel, private_channel"):
+        for channel in result["channels"]:
+            if channel["id"] == config.slack_channel_id:
+                configured_channel = channel
+                break
 
+    if configured_channel is None:
+        raise Exception(f"Unable to find configured slack channel {config.slack_channel_id}")
+    
+    if not configured_channel["is_member"]:
+        raise Exception(f"Slack bot is not a member of configured slack channel {config.slack_channel_id}")
+    
+    if configured_channel["is_archived"]:
+        raise Exception(f"Configured slack channel {config.slack_channel_id} is archived")
+    
 if __name__ == '__main__':
     main()
--- src/config/config.py
+++ src/config/config.py
@@ -1,4 +1,4 @@
-import json
+import json, os
 
 class Config:
     def __init__(self, config_file):
@@ -12,6 +12,16 @@ class Config:
         self.chain_registry_zip_location = None
         if "chain_registry_zip_location" in self.config:
             self.chain_registry_zip_location = self.config["chain_registry_zip_location"]
+
+        if "slack_channel_id" in self.config:
+            self.slack_channel_id = self.config["slack_channel_id"]
+        else:
+            raise Exception("slack_channel_id not set in config")
+
+        self.slack_bot_token = os.environ.get("SLACK_BOT_TOKEN", None)
+
+        if self.slack_bot_token is None:
+            raise Exception("SLACK_BOT_TOKEN environment variable not set")
 
     def load_config(self):
         with open(self.config_file) as f:
